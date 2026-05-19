f4e65342e61488782278509a2e67ffa758e2ae9f
Dipokdas347250 <dipakdas24680@gmail.com>
2026-05-19 09:05:40 +0600

    navber design done

--- src/common/Navber.jsx
+++ src/common/Navber.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import Contaoner from '../container/Contaoner'
+import logo from '../assets/images/logo.png'
+import Navberdata from '../data/navdata'
+import { Link } from 'react-router'
 
 const Navber = () => {
   return (
-   <nav>
+   <nav className='py-4 bg-[#F7FBFF]'>
     <Contaoner>
-         <div>
-        <h3 className='font-nunito text-[30px] font-bold text-primary'>Dipok</h3>
-    </div>
+        <div className="flex items-center justify-between ">
+            <div className="">
+                <Link to="/">
+                    <img src={logo} alt="Logo" width={50} height={50 }  />
+                </Link>
+            </div>
+            <div className="">
+                <ul className='flex gap-10'>
+                    {Navberdata?.navlist?.map((item) => (
+                        <li key={item.id}>
+                            <Link to={item.url}>{item.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     </Contaoner>
    </nav>
   )
