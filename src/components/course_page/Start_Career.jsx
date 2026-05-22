a98261e60954d1a60865959108db328be7e762f3
Dipokdas347250 <dipakdas24680@gmail.com>
2026-05-23 04:07:21 +0600

    Start Your fixed done

--- src/components/course_page/Start_Career.jsx
+++ src/components/course_page/Start_Career.jsx
@@ -13,15 +13,15 @@ const Start_Career = () => {
             <div className="absolute bottom-0 left-0 w-80 h-80 bg-white/10 rounded-full blur-3xl"></div>
     
             <div className="relative max-w-4xl mx-auto text-center">
-              <h2 className="text-4xl md:text-5xl lg:text-6xl text-white mb-6">
+              <h2 className="lg:text-[72px] text-[40px] font-nunito font-bold transition-colors  bg-linear-to-r from-white to-secondary bg-clip-text text-transparent drop-shadow-lg mb-6">
                 Start Your Career Journey Today
               </h2>
-              <p className="text-white/90 text-lg md:text-xl mb-10 leading-relaxed">
+              <p className="text-white/90 text-[20px] font-inter font-normal mb-10 leading-relaxed">
                 Join NextGen Academy and transform your future with
                 industry-leading technical education and practical
                 skills.
               </p>
-              <button className="bg-white text-blue-600 px-10 py-5 rounded-xl text-lg hover:shadow-2xl hover:scale-105 transition-all duration-300 inline-flex items-center gap-3">
+              <button className="bg-white text-primary px-10 py-5 rounded-xl text-[20px] font-inter font-medium hover:shadow-2xl hover:scale-105 transition-all duration-300 inline-flex items-center gap-3">
                 Admission Open
                 <ArrowRight className="w-6 h-6" />
               </button>
